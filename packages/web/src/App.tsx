ce15fe5a63ba903065e8662fc63ca021dadae2de
sreekanth-at-871995868589 <sreekanth@pixellpay.com>
2023-10-05 10:32:13 +0530

    Adding the lint staged step

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Button from "button";
-//import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Button label={"CRA Button"}></Button>
+     
+     
+                 <Button label={"CRA Button"}></Button>
     </div>
   );
 }
