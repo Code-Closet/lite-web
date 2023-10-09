25de6a6f115f9fb67c5b5f67aa0ea96f9d58aadc
sreekanth <sreekanth@pixellpay.com>
2023-10-09 05:40:49 +0000

    Squashed commit of the following
    commit 27239242 
    Author: sreekanth-at-871995868589 <sreekanth@pixellpay.com> 
    Date: Mon Oct 09 2023 11:05:54 GMT+0530 (India Standard Time) 
    
        Added the footer section

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Button from "button";
+import Footer from "footer";
 
 function App() {
   return (
-    <div className="App">
+    <div>
         <Button label={"CRA Button"}></Button>
+        <Footer></Footer>
     </div>
   );
 }
