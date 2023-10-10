4419863e955f730cd9d3b0117656bec80ad1984a
sreekanth-at-871995868589 <sreekanth@pixellpay.com>
2023-10-10 13:07:40 +0530

    Adding login page test cases

--- packages/web/src/pages/__test__/Login.test.tsx
+++ packages/web/src/pages/__test__/Login.test.tsx
@@ -8,4 +8,22 @@ describe("Test suite for Login component", () => {
     const loginElement = screen.getByText(/Login/i);
     expect(loginElement).toBeTruthy();
   });
+
+  it("should render the username input", () => {
+    render(<Login />);
+    const usernameInput = screen.getByText(/Username/i);
+    expect(usernameInput).toBeInTheDocument();
+  });
+
+  it("should render the password input", () => {
+    render(<Login />);
+    const passwordInput = screen.getByText(/Password/i);
+    expect(passwordInput).toBeInTheDocument();
+  });
+
+  it("should render the login button", () => {
+    render(<Login />);
+    const loginButton = screen.getByRole("button");
+    expect(loginButton).toBeInTheDocument();
+  });
 });
