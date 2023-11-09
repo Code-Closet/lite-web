5c2edd5de39e0aa821b943866e83e00f195853fb
sreekanth-at-871995868589 <sreekanth@pixellpay.com>
2023-11-09 10:06:29 +0530

    updated the sonar token

--- sonarqube/sonarscan.js
+++ sonarqube/sonarscan.js
@@ -1,17 +1,17 @@
-const scanner = require('sonarqube-scanner');
+const scanner = require("sonarqube-scanner");
 
 scanner(
-    {
-        serverUrl: 'http://localhost:9000',
-        token: "sqp_211bff6cdcf022629c96a46847776b6931fa97e0",
-        options: {
-            'sonar.projectName': 'pixell-lite-web',
-            'sonar.projectDescription': 'Here I can add a description of my project',
-            'sonar.projectKey': 'pixell-lite-web',
-            'sonar.projectVersion': '0.0.1',
-            'sonar.exclusions': '',
-            'sonar.sourceEncoding': 'UTF-8',
-        }
+  {
+    serverUrl: "http://localhost:9000",
+    token: "squ_93a221bf2dfdeb1a4df4a0edd8f664692e576a12",
+    options: {
+      "sonar.projectName": "pixell-lite-web",
+      "sonar.projectDescription": "Here I can add a description of my project",
+      "sonar.projectKey": "pixell-lite-web",
+      "sonar.projectVersion": "0.0.1",
+      "sonar.exclusions": "",
+      "sonar.sourceEncoding": "UTF-8",
     },
-    () => process.exit()
-)
+  },
+  () => process.exit()
+);
